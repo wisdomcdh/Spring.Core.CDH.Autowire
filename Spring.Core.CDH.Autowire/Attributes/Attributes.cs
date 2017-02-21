bd9e0c207936fe517cd368f09b6d648e2b25a8ff
wisdomcd <wisdomcdh@gmail.com>
2017-02-21 23:52:30 +0900

    최초등록

---
+++ Spring.Core.CDH.Autowire/Attributes/Attributes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Spring.Core.CDH.Autowire
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class AutowireAttribute : Attribute
+    {
+        public string ContextName { get; set; }
+
+        public Type Type { get; set; }
+
+        public bool Singleton { get; set; } = true;
+    }
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class InjectionPropertyAttribute : Attribute
+    {
+        public string PropertyName { get; set; }
+        public string ReferenceContextName { get; set; }
+
+        public InjectionPropertyAttribute(string propertyName, string referenceContextName)
+        {
+            PropertyName = propertyName;
+            ReferenceContextName = referenceContextName;
+        }
+    }
+
+    public class InjectionAdoTemplateAttribute : InjectionPropertyAttribute
+    {
+        public InjectionAdoTemplateAttribute(string referenceContextName = "AdoTemplate") : base("AdoTemplate", referenceContextName)
+        {
+        }
+    }
+}
---
+++ Test/AutowireTest.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using Spring.Context;
+using Spring.Context.Support;
+using Spring.Core.CDH.Autowire;
+using Spring.Data.Generic;
+using System;
+using System.IO;
+
+namespace Test
+{
+    public class AutowireTest
+    {
+        [SetUp]
+        public void OneTimeSetup()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SpringContext.xml");
+            ContextRegistry.Clear();
+            IApplicationContext context = new XmlApplicationContext(false, path);
+            ContextRegistry.RegisterContext(context);
+        }
+
+        [Autowire]
+        public ITestService injectTestService { get; set; }
+
+        [Autowire]
+        public ITestDao injectTestDao { get; set; }
+
+        [Autowire]
+        [InjectionAdoTemplate("AdoTemplate2")]
+        public ITestDao injectTestDao2 { get; set; }
+
+        [Autowire(Type = typeof(TestDao2))]
+        [InjectionProperty("AdoTemplate", "AdoTemplate")]
+        public ITestDao injectTestDao3 { get; set; }
+
+        [Autowire]
+        public ITestService2 injectTestService2 { get; set; }
+
+        [Test]
+        public void Test()
+        {
+            SpringAutowire.Autowire(this);
+        }
+
+        public interface ITestDao { void TestFunction(); }
+
+        public class TestDao : AdoDaoSupport, ITestDao
+        {
+            public void TestFunction()
+            {
+            }
+        }
+
+        public class TestDao2 : ITestDao
+        {
+            public object AdoTemplate { get; set; }
+
+            public void TestFunction()
+            {
+            }
+        }
+
+        public interface ITestService { void TestFunction(); }
+
+        public interface ITestService2 { }
+
+        public interface ITestService3 { }
+
+        public class TestService : ITestService
+        {
+            [Autowire]
+            public ITestDao TestDao { get; set; }
+
+            public void TestFunction()
+            {
+                TestDao.TestFunction();
+            }
+        }
+
+        public class TestService2 : ITestService2
+        {
+            [Autowire]
+            public ITestService3 TestService3 { get; set; }
+        }
+
+        public class TestService3 : ITestService3
+        {
+            [Autowire]
+            public ITestService2 TestService2 { get; set; }
+        }
+    }
+}
