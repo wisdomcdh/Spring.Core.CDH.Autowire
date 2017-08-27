fe3bdaf4ba615a2895580be92ac57db04410971d
wisdomcd <wisdomcdh@gmail.com>
2017-08-27 16:14:00 +0900

    123

---
+++ Spring.Data.CDH/CommonObjectDao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spring.Data.CDH
+{
+    public class CommonObjectDao
+    {
+        //public IList<T> Select<T>()
+        //{
+        //    List<string> a = new List<string>();
+
+
+        //    var b = from aa in a
+        //            select aa[0];
+
+        //    return null;
+        //}
+    }
+}
--- Spring.Data.CDH/Properties/AssemblyInfo.cs
+++ Spring.Data.CDH/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("ea4d7437-ace6-4338-aef0-3df3be28fe7b")]
+[assembly: Guid("110639e0-2b99-439a-9c7e-0b3c61d94d5a")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
